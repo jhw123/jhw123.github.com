535e29168a68c1e24cb2b40471f3e4d3996fe00c
jhw123 <hyoungwookjin123@gmail.com>
2019-06-22 00:47:14 +0900

    use strict mode for js, attribute for translation reduced to one

--- script.js
+++ script.js
@@ -1,22 +1,70 @@
+"use strict";
+
 let curLang='en';
 if(localStorage.getItem("jhwlang"))
 	curLang = localStorage.getItem("jhwlang");
 
-function translate(lang='ko', speed=1000){
+// function translate(lang='ko', speed=1000){
+// 	if(speed === 0){
+// 		for(let elem of document.querySelectorAll(`[${lang}]`))
+// 			elem.innerHTML = elem.getAttribute(`${lang}`);
+// 	}else if(curLang != lang){
+// 		for(let elem of document.querySelectorAll(`[${lang}]`))
+// 			translateElement(lang, elem, speed);
+// 	}
+// 	curLang = lang;
+// 	localStorage.setItem('jhwlang', lang);
+// }
+
+function translate(lang, speed=1000){
 	if(speed === 0){
-		for(let elem of document.querySelectorAll(`[${lang}]`))
-			elem.innerHTML = elem.getAttribute(`${lang}`);
-	}else if(curLang != lang){
-		for(let elem of document.querySelectorAll(`[${lang}]`))
-			translateElement(lang, elem, speed);
+		for(let elem of document.querySelectorAll("[transto]")){
+			const trans_str = elem.getAttribute("transto");
+			let temp_str = elem.textContent.trim();
+			elem.setAttribute("transto", temp_str);
+			elem.innerHTML = trans_str;
+		}
+	}else if(curLang !== lang){
+		for(let elem of document.querySelectorAll("[transto]"))
+			translateElement(elem, speed);
 	}
 	curLang = lang;
 	localStorage.setItem('jhwlang', lang);
 }
 
-function translateElement(transto, elem = document.getElementById("target"), speed){
-	const trans_str = elem.getAttribute(transto);
+// function translateElement(transto, elem = document.getElementById("target"), speed){
+// 	const trans_str = elem.getAttribute(transto);
+// 	let temp_str = elem.textContent.trim();
+
+// 	let candidates = [...Array(Math.min(temp_str.length, trans_str.length)).keys()];
+// 	shuffle(candidates);
+
+// 	let cnt = Math.max(trans_str.length, Math.abs(temp_str.length - trans_str.length));
+// 	const timeInt = speed/cnt;
+
+// 	const loop = setInterval(()=>{
+// 		if(cnt >= 0){
+// 			if(candidates.length > 0){
+// 				idx= candidates.pop();
+// 				temp_str = temp_str.replaceAt(idx, trans_str[idx]);
+// 			}
+// 			if(temp_str.length > trans_str.length)
+// 				temp_str = temp_str.slice(0, -1);
+// 			else if(temp_str.length < trans_str.length)
+// 				temp_str = temp_str+trans_str[temp_str.length];
+// 			elem.innerHTML = temp_str;
+// 			cnt--;
+// 		}else{
+// 			clearInterval(loop);
+// 		}
+// 	}, timeInt);
+// }
+
+function translateElement(elem, speed){
+	const trans_str = elem.getAttribute("transto");
 	let temp_str = elem.textContent.trim();
+
+	elem.setAttribute("transto", temp_str);
 
 	let candidates = [...Array(Math.min(temp_str.length, trans_str.length)).keys()];
 	shuffle(candidates);
@@ -27,7 +75,7 @@ function translateElement(transto, elem = document.getElementById("target"), spe
 	const loop = setInterval(()=>{
 		if(cnt >= 0){
 			if(candidates.length > 0){
-				idx= candidates.pop();
+				const idx= candidates.pop();
 				temp_str = temp_str.replaceAt(idx, trans_str[idx]);
 			}
 			if(temp_str.length > trans_str.length)
@@ -54,6 +102,7 @@ String.prototype.replaceAt=function(index, replacement) {
     return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
 }
 
-translate(curLang, 0);
-document.getElementById('en-btn').onclick = ()=>{translate('en')};
-document.getElementById('ko-btn').onclick = ()=>{translate('ko')};
+if(curLang == "ko")
+	translate("ko", 0);
+document.getElementById('en-btn').onclick = ()=>{translate("en")};
+document.getElementById('ko-btn').onclick = ()=>{translate("ko")};
