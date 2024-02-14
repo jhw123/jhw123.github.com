6855a7d38493b2a532f11cb1bae459bbf90e406e
Hyoungwook Jin <hyoungwookjin123@gmail.com>
2024-02-14 21:29:00 +0900

    🌱 add current project information

--- src/data/project.ts
+++ src/data/project.ts
@@ -16,4 +16,19 @@ class Project {
   }
 }
 
-export const PROJECTS = DataUtil.sortByTime([])
+export const PROJECTS = DataUtil.sortByTime([
+  new Project({
+    title: 'Personalized Teachable Agents',
+    startDate: new Date(2023, 12),
+    imagePath: '/projects/personalized-teachable-agents.png',
+    description:
+      'Teachable agents are pedagogical AI agents that simulate a student role to help learners find gaps in their knowledge by teaching the agents. This project explores how the knowledge level of teachable agents affects the learning experience and aims to develop a technical pipeline for controlling and personalizing the knowledge level of teachable agents to individual learners.',
+  }),
+  new Project({
+    title: 'Helping Students Abstract Subgoals from Code Examples',
+    startDate: new Date(2023, 11),
+    imagePath: '/projects/subgoal-learning.png',
+    description:
+      "This project builds a system that supports programming novices to abstract generalizable subgoals from code examples. We elicit abstract thinking by asking learners to write down subgoals common to interrelated code examples. We use code-generation AI models to create feedback on the generalizability and abstraction level of learners' subgoals by testing if the models can generate code examples from learners' descriptions.",
+  }),
+])
