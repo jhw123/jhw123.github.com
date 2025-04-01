3951e3d44bcd18fc1afb2fd5239dce4b8d86bb32
Hyoungwook Jin <hyoungwookjin123@gmail.com>
2025-04-01 23:02:08 +0900

    🌱 update og preview description

--- src/app/project/teachtune/layout.tsx
+++ src/app/project/teachtune/layout.tsx
@@ -8,9 +8,17 @@ export const metadata: Metadata = {
   keywords: ['Human-AI interaction', 'LLM agents', 'AI and Education', 'Generative AI'],
   openGraph: {
     title: 'TeachTune',
-    description: '',
+    description:
+      'TeachTune is a tool that allows teachers to build pedagogical chatbots for their students and review them through automated conversations between their chatbots and LLM-based simulated students. Our research showed that teachers need to make downstream chatbots for their students and that evaluating learning tools in breadth and depth is crucial before deploying them to actual classes.',
     type: 'website',
     images: [{ url: '/projects/teachtune_og.png', width: 1200, height: 630 }],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'TeachTune',
+    description:
+      'TeachTune is a tool that allows teachers to build pedagogical chatbots for their students and review them through automated conversations between their chatbots and LLM-based simulated students. Our research showed that teachers need to make downstream chatbots for their students and that evaluating learning tools in breadth and depth is crucial before deploying them to actual classes.',
+    images: '/projects/teachtune_og.png',
   },
 }
 
