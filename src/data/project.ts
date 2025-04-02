2789140b89a5b4a64c9e15ec3a221596340364b0
Hyoungwook Jin <hyoungwookjin123@gmail.com>
2025-04-02 11:35:28 +0900

    🌱 update ongoing projects

--- src/data/project.ts
+++ src/data/project.ts
@@ -21,12 +21,19 @@ class Project {
 }
 
 export const PROJECT = {
-  stepwise: new Project({
-    title: 'Helping Students Abstract Subgoals from Code Examples',
-    startDate: new Date(2023, 11),
-    imagePath: '/projects/subgoal-learning.png',
+  teachcraft: new Project({
+    title: 'Helping Teachers Plan, Develop, and Monitor Chatbot-integrated Classes',
+    startDate: new Date(2025, 2),
+    imagePath: '/projects/teachcraft.png',
+    description: `Teachers' interest in using chatbots in their classes is growing, but they often lack the necessary skills and knowledge to effectively integrate them into their teaching. This project aims to build an End-to-End education platform that supports teachers with lesson planning, chatbot development, and real-time monitoring of chatbot usage in the classroom.`,
+  }),
+  mathCog: new Project({
+    title: "Investigating Large Language Models in Diagnosing Students' Cognitive Skills in Math Problem-solving",
+    startDate: new Date(2024, 12),
+    imagePath: '/projects/mathcog.png',
     description:
-      "This project builds a system that supports programming novices to abstract generalizable subgoals from code examples. We elicit abstract thinking by asking learners to write down subgoals common to interrelated code examples. We use code-generation AI models to create feedback on the generalizability and abstraction level of learners' subgoals by testing if the models can generate code examples from learners' descriptions.",
+      "This project investigates whether large language models (LLMs) can diagnose students’ cognitive skills from handwritten math responses. By introducing the MathCog benchmark and evaluating 16 LLMs, we reveal that even advanced models struggle to infer students' thinking process from fragmented visual and textual cues.",
+    link: { Paper: 'https://arxiv.org/abs/2504.00843' },
   }),
 } as const
 export const PROJECTS = DataUtil.sortByTime(Object.values(PROJECT))
