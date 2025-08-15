90e80183213fbe1da968ce9da7ac681f1efea90b
jhw123 <hyoungwookjin123@gmail.com>
2025-08-15 19:42:50 +0900

    ♻️ refactor externallink component

--- src/app/component/externalLink.tsx
+++ src/app/component/externalLink.tsx
@@ -1,20 +1,12 @@
-import styled from '@emotion/styled'
-import { View } from '@wookiejin/react-component'
-
 interface Props {
   href: string
   children: React.ReactNode
 }
 
-export const ExternalLink = View<Props>(({ href, children, forwardedRef, ...props }) => {
+export const ExternalLink = ({ href, children, ...props }: Props) => {
   return (
-    <Container {...props} href={href} target={'_blank'} rel="noreferrer">
+    <a {...props} href={href} target={'_blank'} rel="noopener noreferrer">
       {children}
-    </Container>
+    </a>
   )
-})
-
-const Container = styled.a`
-  display: inline-flex;
-  align-items: center;
-`
+}
